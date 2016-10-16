8763c5ce784328666d00a8d95de5b967a9c7f54c
Sushant Kumar Gupta <sonudoo@users.noreply.github.com>
2016-10-16 16:10:14 +0530

    Update dash.php

--- dash.php
+++ dash.php
@@ -40,7 +40,7 @@ $(function () {
 <?php
 include_once 'dbConnection.php';
 session_start();
-if (!(isset($_SESSION['username']))) {
+if (!(isset($_SESSION['username']))   || ($_SESSION['key']) != '54585c506829293a2d4c3b68543b316e2e7a2d277858545a36362e5f39') {
     header("location:index.php");
 } else {
     $name     = $_SESSION['name'];
