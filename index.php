efabdbc3193d049629eb0c330425de4bbd9a1ed8
Sushant Kumar Gupta <sonudoo@users.noreply.github.com>
2016-10-17 14:54:31 +0530

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ if(isset($_SESSION['username']) && (!isset($_SESSION['key']))){
    header('location:account.php?q=1');
 }
 else if(isset($_SESSION['username']) && isset($_SESSION['key']) && $_SESSION['key'] == '54585c506829293a2d4c3b68543b316e2e7a2d277858545a36362e5f39'){
-   header('location:dash.php?q=1');
+   header('location:dash.php?q=0');
 }
 else{}
 ?>
