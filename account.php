3e4dea0809a95b547abc9ee924901d656bc6bacd
Sushant Kumar Gupta <sonudoo@users.noreply.github.com>
2016-10-17 15:44:26 +0530

    Update account.php

--- account.php
+++ account.php
@@ -575,9 +575,9 @@ if (@$_GET['q'] == 3) {
 <div class="col-md-6 box">
 <span href="#" data-target="#login" style="color:lightyellow">Organized by Quizzer, Institute's Name, Place<br><br></span></div>
 <div class="col-md-2 box">
-<a href="feedback.php" style="color:lightyellow;text-decoration:underline" onmouseover="this.style('color:yellow')">Feedback</a></div>
+<a href="feedback.php" style="color:lightyellow;text-decoration:underline" onmouseover="this.style('color:yellow')" target="new">Feedback</a></div>
 <div class="col-md-2 box">
-<a href="about.php" s style="color:lightyellow;text-decoration:underline" onmouseover="this.style('color:yellow')">About Quizzer</a></div>
+<a href="about.php" s style="color:lightyellow;text-decoration:underline" onmouseover="this.style('color:yellow')" target="new">About Quizzer</a></div>
 </div>
 </body>
 </html>
